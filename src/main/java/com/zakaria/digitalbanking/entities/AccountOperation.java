8930bfbef2ab8e414c03bcdb5d392bd35fe11a9b
ZakariaOuakrim <zakarialukyguy@gmail.com>
2025-05-11 21:21:25 +0100

    entities and enums setup

---
+++ src/main/java/com/zakaria/digitalbanking/entities/AccountOperation.java
@@ -0,0 +1,20 @@
+package com.zakaria.digitalbanking.entities;
+
+import com.zakaria.digitalbanking.enums.OperationType;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.Date;
+@Entity
+@Data @AllArgsConstructor @NoArgsConstructor
+public class AccountOperation {
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private Date operationDate;
+    private double amount;
+    private OperationType operationType;
+    @ManyToOne
+    private BankAccount bankAccount;
+}
---
+++ src/main/java/com/zakaria/digitalbanking/entities/BankAccount.java
@@ -0,0 +1,26 @@
+package com.zakaria.digitalbanking.entities;
+
+import com.zakaria.digitalbanking.enums.AccountStatus;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.Date;
+import java.util.List;
+
+@Entity
+@Inheritance(strategy = InheritanceType.SINGLE_TABLE)
+@DiscriminatorColumn(name = "TYPE", length = 4)
+@Data @AllArgsConstructor @NoArgsConstructor
+public class BankAccount {
+    @Id
+    private String id;
+    private  double balance;
+    private Date createdAt;
+    private AccountStatus status;
+    @ManyToOne
+    private Customer customer;
+    @OneToMany(mappedBy = "bankAccount")
+    private List<AccountOperation> accountOperation;
+}
